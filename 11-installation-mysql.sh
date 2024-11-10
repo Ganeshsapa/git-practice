dc232d22fc145495a92e5fba01ca344b5ed8355d
GaneshS <ganvic123@gmail.com>
2024-11-10 20:14:56 +0530

    functions

--- 11-installation-mysql.sh
+++ 11-installation-mysql.sh
@@ -9,3 +9,13 @@ exit 1
 fi
 
 dnf install git -y
+
+if [ $? -ne 0 ]
+
+then
+    echo "Git is not installed,going to install it.."
+    dnf install git -y
+    
+else
+    echo "Git is already installed, nothing to do.."
+    fi
