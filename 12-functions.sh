909576b09d912fb7e3b187f3c79dcb3183ab95e7
GaneshS <ganvic123@gmail.com>
2024-11-10 20:45:57 +0530

    functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -10,12 +10,14 @@ VALIDATE()
         echo "exit status: $1"
 }
 
-# if [ $USERID -ne 0 ]
-# then echo "Please run this script with root priveleges"
-# exit 1
-# fi
+if [ $USERID -ne 0 ]
+then echo "Please run this script with root priveleges"
+exit 1
+fi
 
-# dnf list installed git
+dnf list installed git
+
+VALIDATE
 
 # if [ $? -ne 0 ]
 
