67ad8f482ed67b4d4c6585469e0a6dc49b642a51
GaneshS <ganvic123@gmail.com>
2024-11-10 10:31:17 +0530

    added 02 conversation

---
+++ 02-conersation.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "Rakesh:Hi Ramesh, How are you ? "
+echo "Suresh: Hello Rakesh, I am fine, How are you ?"
+echo "Rakesh: I am doing good, what's going on"
+echo  "Suresh: I am learning devops"
