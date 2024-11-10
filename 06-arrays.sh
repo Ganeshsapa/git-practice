3874269f097cae39a5f054812097a464f21880c5
GaneshS <ganvic123@gmail.com>
2024-11-10 13:39:02 +0530

    varialbles

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #index starts from 0, size is 3
-FRUITS={"APPLE", "BANANA","GRAPHS"}
+FRUITS=("APPLE", "BANANA","GRAPHS")
 
 echo "First fruit is :${FRUITS[0]}"
-echo "First fruit is :${FRUITS[1]}"
-echo "First fruit is :${FRUITS[2]}"
-echo "First fruit is :${FRUITS[@]}"
+echo "Second fruit is :${FRUITS[1]}"
+echo "Third fruit is :${FRUITS[2]}"
+echo "All fruit is :${FRUITS[@]}"
