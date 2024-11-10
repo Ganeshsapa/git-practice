32fe12eb129bf5079b61119afa5f2caa8e09246b
GaneshS <ganvic123@gmail.com>
2024-11-10 13:25:01 +0530

    varialbles

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,4 +3,9 @@
 echo " Please enter your username:"
 read USERNAME
 
-echo "Username entered is : $USERNAME"
+echo "Username entered is : $Password"
+
+echo " Please enter your username:"
+read PASSWORD
+
+echo "Username entered is : $PASSWORD"
