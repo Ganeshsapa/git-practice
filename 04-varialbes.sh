37c6d3608911fba56747b406b8d929ae4155f6a4
GaneshS <ganvic123@gmail.com>
2024-11-10 13:15:12 +0530

    variables

---
+++ 04-varialbes.sh
@@ -0,0 +1,7 @@
+PERRSON1=$1
+PERRSON2=$2
+
+echo "$PERRSON1:Hi ${PERRSON2}, How are you ? "
+echo "${PERRSON2}: Hello Rakesh, I am fine, How are you ?"
+echo "PERRSON1: I am doing good ${PERRSON2}, what's going on"
+echo  "PERRSON2: I am learning devops $PERRSON1"
